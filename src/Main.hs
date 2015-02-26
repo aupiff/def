3e644ec32e3ae1ca40493eefbb4eaa5c021fbf85
Roy Blankman <riblankman@gmail.com>
2015-02-25 21:04:58 -0800

    qualifying imports

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,16 @@
 {-# LANGUAGE ExistentialQuantification #-}
 
 import qualified Network.HTTP.Base as N
-import Network.HTTP.Conduit (simpleHttp)
+import qualified Network.HTTP.Conduit as NHC
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
-import Text.XML
-import Data.Text.Encoding
-import Text.XML.Cursor (Cursor, attribute, attributeIs, content, laxElement, element, fromDocument, child,
-                        ($//), ($.//), (&|), (&.//), (&//), (&/), (>=>))
+import Text.XML as TX
+import Text.XML.Cursor (Cursor, ($//), ($.//), (&|), (&.//), (&//), (&/), (>=>))
+import qualified Text.XML.Cursor as TXC
 import Control.Exception
-import Data.Maybe (listToMaybe)
-import Data.Default
-import Control.Applicative
+import qualified Data.Maybe as M
+import Data.Default as DD
+import Control.Applicative ((<$>))
 
 prompt :: IO String
 prompt = do putStr "Enter a word: "
@@ -31,25 +30,25 @@ isFrench :: Cursor -> Bool
 isFrench = undefined
 
 findTextNode :: Cursor -> [T.Text]
-findTextNode = element "text" >=> child >=> content
+findTextNode = TXC.element "text" >=> TXC.child >=> TXC.content
 
 -- TODO it'd be nice to have simpleHttp return an Either type
 cursorFor :: String -> IO (Either SomeException Cursor)
-cursorFor url = do
-     page <- simpleHttp url
-     return $ fromDocument <$> parseLBS def page
+cursorFor url = do page <- NHC.simpleHttp url
+                   return $ TXC.fromDocument <$> TX.parseLBS DD.def page
 
 maybeToEither = flip maybe Right . Left
 
 wikiContentCursorFor :: Cursor -> Either SomeException Cursor
-wikiContentCursorFor cursor = fmap fromDocument . parseText def . TL.fromStrict =<< textNodeE
-                              where noTextException =  toException $ IndexOutOfBounds "no text element found"
-                                    wrapRoot x = T.append (T.pack "<root>") $ T.append x (T.pack "</root>")
-                                    textNodeM = fmap wrapRoot . listToMaybe $ cursor $.// findTextNode
-                                    textNodeE = maybeToEither noTextException textNodeM
+wikiContentCursorFor cursor =
+    fmap TXC.fromDocument . TX.parseText DD.def . TL.fromStrict =<< textNodeE
+    where noTextException =  toException $ IndexOutOfBounds "no text element found"
+          wrapRoot x = T.append (T.pack "<root>") $ T.append x (T.pack "</root>")
+          textNodeM = fmap wrapRoot . M.listToMaybe $ cursor $.// findTextNode
+          textNodeE = maybeToEither noTextException textNodeM
 
 definitionContentCursor :: Cursor -> [Cursor]
-definitionContentCursor cursor = cursor $.// element "h2"
+definitionContentCursor cursor = cursor $.// TXC.element "h2"
 
 main :: IO ()
 main = do
