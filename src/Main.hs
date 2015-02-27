cde130477026608808fef9042e6b15ba606c4cdf
Roy Blankman <riblankman@gmail.com>
2015-02-27 13:32:01 -0800

    cleaning up imports

--- src/Main.hs
+++ src/Main.hs
@@ -5,24 +5,25 @@ import qualified Network.HTTP.Base as N
 import qualified Network.HTTP.Conduit as NHC
 import Control.Applicative ((<$>))
 
-import Definition
-import Language
-import Parse
+import qualified Definition
+import qualified Language
+import qualified Parse
 
 prompt :: IO String
 prompt = do putStrLn "Enter a word: "
             getLine
 
-dictionaryOutput :: forall t. Either t Definition -> IO ()
+dictionaryOutput :: forall t. Either t Definition.Definition -> IO ()
 dictionaryOutput (Left _) = putStrLn "definition not found"
-dictionaryOutput (Right d) = prettyPrintDefinition d
+dictionaryOutput (Right d) = Definition.prettyPrintDefinition d
 
 baseUrl :: String
-baseUrl = "http://" ++ langCode destinationLang ++ ".wiktionary.org/w/api.php?action=parse&format=xml&prop=text|revid|displaytitle&callback=?&page="
+baseUrl = "http://" ++ cname ++ ".wiktionary.org/w/api.php?action=parse&format=xml&prop=text|revid|displaytitle&callback=?&page="
+    where cname = Language.langCode Language.destinationLang
 
 main :: IO ()
 main = do word <- N.urlEncode <$> prompt
           page <- NHC.simpleHttp $ baseUrl ++ word
-          let cc = pageToDefinition page
+          let cc = Parse.pageToDefinition page
           dictionaryOutput cc
           main
